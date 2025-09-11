5c9e977e9097172656fc5c22b0128b1f50a8e062
mrtrq <tarreq.maulana@gmail.com>
2025-09-11 09:09:32 +0700

    removing excessive underlines

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,9 +1,15 @@
-export function ProjectCard({ title, desc, href }: { title: string; desc: string; href: string }) {
-return (
-    <a href={href} className="block border border-[--color-fg] p-6 hover:bg-black hover:text-white hover:-translate-y-[2px] focus-visible:outline focus-visible:outline-black transition-colors">
-    <div className="uppercase text-xs tracking-[0.2em] mb-2">Project</div>
-    <h3 className="mb-1">{title}</h3>
-    <p className="text-sm text-balance">{desc}</p>
+import { NonBinary } from "lucide-react";
+
+export function ProjectCard({ topic, title, desc, href }: { topic: string, title: string; desc: string; href: string }) {
+  return (
+    <a
+      href={href}
+      style={{textDecoration: 'none'}}
+      className="block border border-[--color-fg] no-underline p-6 hover:bg-black hover:text-white hover:-translate-y-[2px] focus-visible:outline focus-visible:outline-black transition-colors"
+    >
+      <div className="uppercase text-xs tracking-[0.2em] mb-2">{topic}</div>
+      <h3 className="mb-1 underline">{title}</h3>
+      <p className="text-sm text-balance">{desc}</p>
     </a>
-);
+  );
 }
