014376ffb0f0164c732f9700d179967e79893481
mrtrq <tarreq.maulana@gmail.com>
2025-09-11 11:35:36 +0700

    modify socials on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,6 @@ return (
             <p className="medium">© {new Date().getFullYear()} Muhammad Tarreq</p>
             </div>
         <div className="col-span-12 md:col-span-4 flex gap-4 justify-start md:justify-end">
-            <a href="https://x.com/plandemic_" aria-label="X">X</a>
             <a href="https://github.com/mrtrq" aria-label="GitHub">GitHub</a>
             <a href="mailto:tarreq.maulana@gmail.com" aria-label="Email">Email</a>
         </div>
